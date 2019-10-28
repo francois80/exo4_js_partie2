7de4abf9b59e35cb5d83fcf26331956b5d9905be
francois80 <glavieux.francois@orange.fr>
2019-10-28 16:47:24 +0100

    ajout exo 4 javascript parie 2

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener('click',fEfface);
+
+function fEfface(){
+  document.getElementById('lastname').value="";
+  document.getElementById('firstname').value="";
+  document.getElementById('city').value="";
+}
